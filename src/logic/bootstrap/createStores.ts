9e6e11f656ab568722830364b38b9b66448739ae
Barin Britva <barinbritva@outlook.com>
2020-09-17 18:57:13 +0300

    Move user from stores.

--- src/logic/bootstrap/createStores.ts
+++ src/logic/bootstrap/createStores.ts
@@ -2,20 +2,13 @@ import {FeedStore} from '../store/FeedStore';
 import {FilterStore} from '../store/FilterStore';
 import {RepositoryComposite} from '../service/RepositoryComposite';
 import {FacebookRepository} from '../repositories/FacebookRepository';
-import {User} from '../interfaces/User';
 
-export function createStores(): [FeedStore, FilterStore, User] {
+export function createStores(): [FeedStore, FilterStore] {
   const feedStore = new FeedStore(new RepositoryComposite([new FacebookRepository()]));
   const filterStore = new FilterStore(feedStore);
-  const user: User = {
-    name: 'Ali Ragimov',
-    avatar: 'https://instagram.fhel5-1.fna.fbcdn.net/v/t51.2885-19/s320x320/109299228_225454511915197_9050500074834384651_n.jpg?_nc_ht=instagram.fhel5-1.fna.fbcdn.net&_nc_ohc=Gg97jrW8X-UAX8TM4QZ&oh=2a19cd81174c8611c7887c72352c4181&oe=5F8A920C',
-    username: 'barinbrtiva'
-  }
 
   return [
     feedStore,
-    filterStore,
-    user
+    filterStore
   ];
 }
--- test/createStores.ts
+++ test/createStores.ts
@@ -2,21 +2,13 @@ import {FeedStore} from '../src/logic/store/FeedStore';
 import {FilterStore} from '../src/logic/store/FilterStore';
 import {RepositoryComposite} from '../src/logic/service/RepositoryComposite';
 import {FakeRepository} from './FakeRepository';
-import {User} from '../src/logic/interfaces/User';
 
-export function createStores(): [FeedStore, FilterStore, User] {
+export function createStores(): [FeedStore, FilterStore] {
   const feedStore = new FeedStore(new RepositoryComposite([new FakeRepository()]));
   const filterStore = new FilterStore(feedStore);
 
-  const user: User = {
-    name: 'Jhon Doe',
-    avatar: 'https://example.com/avatar.jpg',
-    username: 'jhon-doe'
-  }
-
   return [
     feedStore,
-    filterStore,
-    user
+    filterStore
   ];
 }
